9a969dd808a65142edbf89ba23b2459c0a1a3af0
Rasso Hilber <mail@rassohilber.com>
2023-06-15 12:44:57 +0200

    Update logging prefix

--- src/inc/utils.ts
+++ src/inc/utils.ts
@@ -1,3 +1,3 @@
 export function log(message: string, context: any, type: 'log' | 'warn' | 'error' = 'log') {
-	console[type](`[swup-fragment-plugin] ${message}`, context);
+	console[type](`[@swup/fragment-plugin] ${message}`, context);
 }
