e656d9e2f1a7e581f7f24e356fe83015078a13a5
Rasso Hilber <mail@rassohilber.com>
2023-06-22 10:19:51 +0200

    Lint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import FragmentPlugin from "./inc/FragmentPlugin.js";
-import type { Rule, Route } from "./inc/FragmentPlugin.js";
+import FragmentPlugin from './inc/FragmentPlugin.js';
+import type { Rule, Route } from './inc/FragmentPlugin.js';
 export default FragmentPlugin;
 export { Rule as FragmentRule, Route as FragmentRoute };
