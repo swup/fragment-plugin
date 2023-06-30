1a6a1aac2a400db940d9533e39f4dfaa4e647ae0
Rasso Hilber <mail@rassohilber.com>
2023-06-30 13:11:40 +0200

    Clean-up Rule

--- src/inc/Rule.ts
+++ src/inc/Rule.ts
@@ -1,30 +1,31 @@
-import { matchPath, Path } from './matchPath.js';
+import { matchPath, type Path } from './matchPath.js';
 import type { Route } from './FragmentPlugin.js';
+
 export type { Path };
 
 /**
  * Represents a Rule
  */
 export default class Rule {
-	fragments: string[] = [];
-	name: string | undefined;
 
-	matchesFrom;
-	matchesTo;
+	protected matchesFrom;
+	protected matchesTo;
 
-	constructor(from: Path, to: Path, fragments: string[], name: string | undefined) {
-		this.name = name;
-
+	constructor(
+		public from: Path,
+		public to: Path,
+		public fragments: string[],
+		public name: string | undefined
+	) {
 		this.matchesFrom = matchPath(from);
 		this.matchesTo = matchPath(to);
-
 		this.fragments = fragments.map((selector) => selector.trim());
 	}
 
 	/**
 	 * Checks if a given route matches a this rule
 	 */
-	matches(route: Route): boolean {
+	public matches(route: Route): boolean {
 		return this.matchesFrom(route.from) !== false && this.matchesTo(route.to) !== false;
 	}
 }
