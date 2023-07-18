4e0fbe1a6ec6aa4787f1bec00f6dc343472abd84
Rasso Hilber <mail@rassohilber.com>
2023-07-18 11:19:50 +0200

    Validate fragment selectors

--- src/inc/Rule.ts
+++ src/inc/Rule.ts
@@ -1,5 +1,6 @@
 import { matchPath, classify, type Path } from 'swup';
 import type { Route } from '../SwupFragmentPlugin.js';
+import type Logger from './Logger.js';
 import { dedupe } from './functions.js';
 /**
  * Represents a Rule
@@ -12,12 +13,14 @@ export default class Rule {
 	to: Path;
 	fragments: string[];
 	name?: string;
+	logger?: Logger;
 
-	constructor(from: Path, to: Path, rawFragments: string[], name?: string) {
+	constructor(from: Path, to: Path, rawFragments: string[], name?: string, logger?: Logger) {
 		this.from = from;
 		this.to = to;
 		this.matchesFrom = matchPath(from);
 		this.matchesTo = matchPath(to);
+		this.logger = logger;
 		this.fragments = this.parseFragments(rawFragments);
 		if (name) this.name = classify(name);
 	}
@@ -28,7 +31,23 @@ export default class Rule {
 	parseFragments(rawFragments: string[]): string[] {
 		// trim selectors
 		const fragments = rawFragments.map((selector) => selector.trim());
+		fragments.forEach((selector) => {
+			const result = this.validateSelector(selector);
+			if (result !== true) this.logger?.error(result);
+		});
 		return dedupe(fragments);
+	}
+
+	/**
+	 * Validate a fragment selector
+	 *
+	 * - only IDs are allowed
+	 * - no nested selectors
+	 */
+	validateSelector(selector: string): true | string {
+		if (!selector.startsWith('#')) return `fragment selectors must be IDs: ${selector}`;
+		if (selector.match(/\s|>/)) return `fragment selectors must not be nested: ${selector}`;
+		return true;
 	}
 
 	/**
