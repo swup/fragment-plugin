5455d763acbbe6b17962718f0d69fa297c5f912e
Rasso Hilber <mail@rassohilber.com>
2023-07-23 13:58:04 +0200

    Add more rule validation

--- src/inc/Rule.ts
+++ src/inc/Rule.ts
@@ -16,12 +16,18 @@ export default class Rule {
 	name?: string;
 
 	constructor(from: Path, to: Path, rawContainers: string[], name?: string, logger?: Logger) {
-		this.from = from;
-		this.to = to;
+		this.from = from || '';
+		this.to = to || '';
+
+		if (name) this.name = classify(name);
+
+		this.containers = this.parseContainers(rawContainers, logger);
+
+		if (!to) logger?.error(`Every fragment rule must contain a 'to' path`, this);
+		if (!from) logger?.error(`Every fragment rule must contain a 'from' path`, this);
+
 		this.matchesFrom = matchPath(from);
 		this.matchesTo = matchPath(to);
-		this.containers = this.parseContainers(rawContainers, logger);
-		if (name) this.name = classify(name);
 	}
 
 	/**
@@ -30,7 +36,8 @@ export default class Rule {
 	parseContainers(rawContainers: string[], logger?: Logger): string[] {
 		if (!Array.isArray(rawContainers)) {
 			logger?.error(`Every fragment rule must contain an array of containers`, this);
-		}
+			return [];
+		};
 		// trim selectors
 		const containers = rawContainers.map((selector) => selector.trim());
 		containers.forEach((selector) => {
