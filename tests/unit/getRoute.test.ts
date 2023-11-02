4b172aa22608cd0ccf9a6fcbedc006242a42067f
Rasso Hilber <mail@rassohilber.com>
2023-11-02 10:07:53 +0100

    Add tests for `getRoute()`

---
+++ tests/unit/getRoute.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it, vi } from 'vitest';
+import { getRoute } from '../../src/inc/functions.js';
+import Swup from 'swup';
+
+describe('getRoute()', () => {
+	it('should get the route from a visit', () => {
+		const swup = new Swup();
+		const route = { from: '/page-1/', to: '/page-2/' };
+		// @ts-expect-error createVisit is protected
+		const visit = swup.createVisit(route);
+		expect(getRoute(visit)).toEqual(route);
+	});
+	it('should return undefined for incomplete visits', () => {
+		const swup = new Swup();
+		// @ts-expect-error createVisit is protected
+		const withoutTo = swup.createVisit({ to: '' });
+		expect(getRoute(withoutTo)).toEqual(undefined);
+
+		// @ts-expect-error createVisit is protected
+		const withoutFrom = swup.createVisit({ from: '', to: '/page-2/' });
+		expect(getRoute(withoutFrom)).toEqual(undefined);
+	});
+});
