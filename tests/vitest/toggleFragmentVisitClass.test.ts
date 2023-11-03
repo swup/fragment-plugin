02255a6a90f8a1266b3a4a20eae7e9c089cec139
Rasso Hilber <mail@rassohilber.com>
2023-11-03 12:04:54 +0100

    formatting

--- tests/vitest/toggleFragmentVisitClass.test.ts
+++ tests/vitest/toggleFragmentVisitClass.test.ts
@@ -1,24 +1,22 @@
 import { describe, expect, it } from 'vitest';
 import { toggleFragmentVisitClass } from '../../src/inc/functions.js';
 import { stubGlobalDocument } from './inc/helpers.js';
-import Swup from 'swup';
 import { FragmentVisit } from '../../src/index.js';
 
 describe('toggleFragmentVisitClass()', () => {
-	it('should toggle a fragment visit\'s name on the classList of all fragment elements', () => {
-		stubGlobalDocument(
-			/*html*/ `
+	it("should toggle a fragment visit's name on the classList of all fragment elements", () => {
+		stubGlobalDocument(/*html*/ `
 			<div id="swup">
 				<div id="fragment-1"></div>
 				<div id="fragment-2"></div>
-			</div>`
-		);
-		const swup = new Swup();
-		const fragmentVisit:FragmentVisit = {
+			</div>`);
+
+		const fragmentVisit: FragmentVisit = {
 			name: 'test',
 			containers: ['#fragment-1', '#fragment-2'],
 			scroll: false
-		}
+		};
+
 		toggleFragmentVisitClass(fragmentVisit, true);
 		expect(document.querySelector('#fragment-1')?.classList.contains(`to-test`)).toBe(true);
 		expect(document.querySelector('#fragment-2')?.classList.contains(`to-test`)).toBe(true);
@@ -28,18 +26,17 @@ describe('toggleFragmentVisitClass()', () => {
 		expect(document.querySelector('#fragment-2')?.classList.contains(`to-test`)).toBe(false);
 	});
 
-	it('should\'t add classes if the current fragment visit doesn\'t have a \`name\`', () => {
-		stubGlobalDocument(
-			/*html*/ `
+	it("should't add classes if the current fragment visit doesn't have a `name`", () => {
+		stubGlobalDocument(/*html*/ `
 			<div id="swup">
 				<div id="fragment-1"></div>
-			</div>`
-		);
-		const swup = new Swup();
-		const fragmentVisit:FragmentVisit = {
+			</div>`);
+
+		const fragmentVisit: FragmentVisit = {
 			containers: ['#fragment-1', '#fragment-2'],
 			scroll: false
-		}
+		};
+
 		toggleFragmentVisitClass(fragmentVisit, true);
 		expect(document.querySelector('#fragment-1')?.classList.contains(`to-test`)).toBe(false);
 	});
