07605f3cdbf19072f24192534dc5e61abc0652f5
Rasso Hilber <mail@rassohilber.com>
2023-11-10 13:01:54 +0100

    Optimize test for `cloneRules`

--- tests/vitest/cloneRules.test.ts
+++ tests/vitest/cloneRules.test.ts
@@ -11,18 +11,16 @@ describe('cloneRules()', () => {
 		const reference = original;
 		expect(original === reference).toEqual(true);
 
-		/** Test shallow copy */
+		/** test shallow copy */
 		const shallowCopy = { ...original };
+		expect(original === shallowCopy).toEqual(false);
 		expect(original[0].containers === shallowCopy[0].containers).toEqual(true);
 		expect(original[1].from === shallowCopy[1].from).toEqual(true);
 
-		/** test clone */
+		/** test deep clone */
 		const clone = cloneRules(original);
 		expect(original === clone).toEqual(false);
 		expect(original[0].containers === clone[0].containers).toEqual(false);
 		expect(original[1].from === clone[1].from).toEqual(false);
-
-		/** make sure the containers are also cloned */
-		expect(original[0].containers === clone[0].containers).toEqual(false);
 	});
 });
