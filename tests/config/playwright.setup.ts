4d448a37cf2f411cbde0e77be7e3dd52fa1c2b23
Rasso Hilber <mail@rassohilber.com>
2023-11-13 17:02:30 +0100

    ignore missing dist folder

--- tests/config/playwright.setup.ts
+++ tests/config/playwright.setup.ts
@@ -1,11 +1,10 @@
-import { mkdirSync, cpSync, rmSync } from 'node:fs';
+import { cpSync, rmSync } from 'node:fs';
 
 const options = { recursive: true };
 const fixturesDir = './tests/fixtures';
 
 export default () => {
-	rmSync('./tests/fixtures/dist/', options);
-	mkdirSync('./tests/fixtures/dist');
+	rmSync('./tests/fixtures/dist/', { ...options, force: true });
 	// cpSync('./node_modules/', `${fixturesDir}/dist/node_modules/`, options);
 	// cpSync('./dist/', `${fixturesDir}/dist/fragment-plugin/`, options);
 	cpSync('./node_modules/swup/dist/Swup.umd.js', `${fixturesDir}/dist/swup.umd.js`);
