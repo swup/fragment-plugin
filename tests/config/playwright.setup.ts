04f3d1ec86d524e8dfa118dc1a059dbe30e06198
Rasso Hilber <mail@rassohilber.com>
2023-11-13 17:00:24 +0100

    make sure `tests/fixtures/dist/` exists

--- tests/config/playwright.setup.ts
+++ tests/config/playwright.setup.ts
@@ -1,10 +1,11 @@
-import { cpSync, rmSync } from 'node:fs';
+import { mkdirSync, cpSync, rmSync } from 'node:fs';
 
 const options = { recursive: true };
 const fixturesDir = './tests/fixtures';
 
 export default () => {
 	rmSync('./tests/fixtures/dist/', options);
+	mkdirSync('./tests/fixtures/dist');
 	// cpSync('./node_modules/', `${fixturesDir}/dist/node_modules/`, options);
 	// cpSync('./dist/', `${fixturesDir}/dist/fragment-plugin/`, options);
 	cpSync('./node_modules/swup/dist/Swup.umd.js', `${fixturesDir}/dist/swup.umd.js`);
