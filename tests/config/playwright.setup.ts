8fa0d55f990befae535960bba6dc29ad3d3164e8
Rasso Hilber <mail@rassohilber.com>
2023-11-13 17:02:47 +0100

    cleanup

--- tests/config/playwright.setup.ts
+++ tests/config/playwright.setup.ts
@@ -5,8 +5,6 @@ const fixturesDir = './tests/fixtures';
 
 export default () => {
 	rmSync('./tests/fixtures/dist/', { ...options, force: true });
-	// cpSync('./node_modules/', `${fixturesDir}/dist/node_modules/`, options);
-	// cpSync('./dist/', `${fixturesDir}/dist/fragment-plugin/`, options);
 	cpSync('./node_modules/swup/dist/Swup.umd.js', `${fixturesDir}/dist/swup.umd.js`);
 	cpSync('./dist/index.umd.js', `${fixturesDir}/dist/fragment-plugin.umd.js`);
 };
