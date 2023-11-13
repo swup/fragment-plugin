1d9bcb69ad76922558507ad223f053cdc0ec3c5a
Rasso Hilber <mail@rassohilber.com>
2023-11-13 14:48:01 +0100

    Add first playwright tests

--- tests/playwright/fragment-visit.spec.ts
+++ tests/playwright/fragment-visit.spec.ts
@@ -7,11 +7,33 @@ import { prefixed } from './inc/utils.js';
 const url = prefixed('/');
 
 test.describe('replace fragments', () => {
-	test.beforeEach(async ({ page }) => {
-		await page.goto('/list/');
+	test.beforeEach(async ({ page }) => {});
+
+	test('should ignore visits without a matching rule', async ({ page }) => {
+		await page.goto('/');
+		const swupID = await page.locator('#swup').getAttribute('data-uniqueid');
+		await clickOnLink(page, '/list/');
+		await expectToBeAt(page, '/list/', 'List');
+		/* #swup was replaced */
+		await expect(page.locator('#swup')).not.toHaveAttribute('data-uniqueid', String(swupID));
 	});
 
-	test('should be at home', async ({ page }) => {
-		await expectToBeAt(page, '/list/', 'List');
+	test('should replace fragments', async ({ page }) => {
+		await page.goto('/list/');
+		const swupID = await page.locator('#swup').getAttribute('data-uniqueid');
+		await clickOnLink(page, '/list/filter/red/');
+		await expectToBeAt(page, '/list/filter/red/');
+		await expect(page.locator('h2')).toHaveText('Red Items');
+		/* #swup was left alone */
+		await expect(page.locator('#swup')).toHaveAttribute('data-uniqueid', String(swupID));
+	});
+
+	test('should cache foreign fragments', async ({ page }) => {
+		await page.goto('/list/');
+		await clickOnLink(page, '/list/filter/green/');
+		await clickOnLink(page, '/detail/');
+		await clickOnLink(page, '/');
+		await page.goBack();
+		await expect(page.locator('#list h2')).toHaveText('Green Items');
 	});
 });
