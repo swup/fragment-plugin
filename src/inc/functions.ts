cd8720f34b12b4acf0530e1d9f1e76f2f17a52b3
Rasso Hilber <mail@rassohilber.com>
2025-02-04 20:58:58 +0100

    Don't assume showModal exists – otherwise, unit tests will fail

--- src/inc/functions.ts
+++ src/inc/functions.ts
@@ -32,7 +32,8 @@ function showDialogs({ logger }: FragmentPlugin): void {
 			if (el.__swupFragment.modalShown) return;
 			el.__swupFragment.modalShown = true;
 			el.removeAttribute('open');
-			el.showModal();
+			/** don't assume showModal exists – otherwise unit tests will fail */
+			el.showModal?.();
 			el.focus();
 			el.addEventListener('keydown', (e) => e.key === 'Escape' && e.preventDefault());
 		});
