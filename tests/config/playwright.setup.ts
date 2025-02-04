835ef1ca4fe73e7c3d98ac44ac8d7924ed3e736f
Rasso Hilber <mail@rassohilber.com>
2025-02-04 20:51:20 +0100

    Optimize test preparation

--- tests/config/playwright.setup.ts
+++ tests/config/playwright.setup.ts
@@ -1,10 +1,5 @@
-import { cpSync, rmSync } from 'node:fs';
-
-const options = { recursive: true };
-const fixturesDir = './tests/fixtures';
+import { prepare } from '../prepare.js';
 
 export default () => {
-	rmSync('./tests/fixtures/dist/', { ...options, force: true });
-	cpSync('./node_modules/swup/dist/Swup.umd.js', `${fixturesDir}/dist/swup.umd.js`);
-	cpSync('./dist/index.umd.js', `${fixturesDir}/dist/fragment-plugin.umd.js`);
+	prepare();
 };
