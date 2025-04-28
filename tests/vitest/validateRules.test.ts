c57cdc711d311e7c4cc3647e18b33edb696663ef
Rasso Hilber <mail@rassohilber.com>
2025-04-28 21:03:18 +0200

    remove `.only(` 🤦‍♂️

--- tests/vitest/validateRules.test.ts
+++ tests/vitest/validateRules.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 import { validateRules } from '../../src/inc/functions.js';
 
 describe('validateRules()', () => {
-	it.only('should remove invalid rules', () => {
+	it('should remove invalid rules', () => {
 		expect(validateRules([{ from: '', to: '', containers: ['#foobar'] }])).toEqual([]);
 
 		expect(validateRules([{ from: '', to: 'bar', containers: ['#foobar'] }])).toEqual([]);
